e2b3c8eef469ed25d88e2994f72427b4343a269e
obemauricio <mauricioobey@gmail.com>
2021-03-18 15:11:51 -0400

    a medias

--- Estadistica_inferencial/estadistica.py
+++ Estadistica_inferencial/estadistica.py
@@ -1,13 +1,30 @@
 import random
+import math
 
 def media(x):
     return sum(x) / len(x)
 
+def varianza(x):
+    mu = media(x)
+
+    acumulador = 0
+    for X in x:
+        acumulador += (X - mu)**2
+
+    return acumulador / len(x)
+
+def desviacion_estandar(x):
+    return math.sqrt(varianza(x))
+
 if __name__ == '__main__':
     x = [random.randint(1,21) for i in range(20)]
     
-    print(x)
     mu = media(x)
+    Var = varianza(x)
+    sigma = desviacion_estandar(x)
     
 
-    print(mu)
+    print(f'Arreglo X: {x}')
+    print(f'Media = {mu}')
+    print(f'Varianza = {Var}')
+    print(f'Desviacion Estandar = {sigma}')
